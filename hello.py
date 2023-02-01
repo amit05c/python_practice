198eda56d9de5b48e2870e66f4b50ce3e3838e3c
amig05c <amitghosh0595c@gmail.com>
2023-02-01 23:00:49 +0530

    python practice

---
+++ hello.py
@@ -0,0 +1,65 @@
+# if 5 > 2:
+#    print("Five is greater than two!")
+
+
+# if 5 > 2:
+#  print("Five is greater than two!") 
+# if 5 > 2:
+#     print("Five is greater than two!") 
+
+# if 5 > 2:
+#  print("Five is greater than two!")
+#  print("Five is greater than two!")
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = 4       # x is of type int
+# x = "Sally" # x is now of type str
+# print(x)
+
+
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+# (green, yellow, *red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x+y+x)
+
+
+# x = 5
+# y = 10
+# print(x + y)
+
+# x = 5
+# y = "amit"
+# print(x + y)
+
+
+
+# x=5 
+# def myfunc():
+#     print("python is ",x)
+
+# myfunc()
+
+# for x in "banana":
+#   print(x)
+
+
+a = "Hello, World!"
+print(len(a))
